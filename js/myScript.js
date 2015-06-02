775523e31c48f3e43b4456b36a3a2b1558d251d3
sneha Vankireddy <vankireddy.s@husky.neu.edu>
2015-06-02 10:53:55 -0400

    Indentation html,js
    Indentation html,js

--- js/myScript.js
+++ js/myScript.js
@@ -1,51 +1,51 @@
 var images = '.image-container',
-  slide = $(images + ' img').innerWidth();
+    slide = $(images + ' img').innerWidth();
 
 function append() {
-  $(images + ' img').first().appendTo($(images));
+    $(images + ' img').first().appendTo($(images));
 }
 
 function prepend() {
-  $(images + ' img').last().prependTo($(images));
+    $(images + ' img').last().prependTo($(images));
 }
 
 $('.prev').click(function() {
-  prepend();
+    prepend();
 });
 $('.next').click(function() {
-  append();
+    append();
 });
 
 function startTimer() {
-  setInterval(append, 9000);
+    setInterval(append, 9000);
 }
-$(document).ready(function () {
+$(document).ready(function() {
     $('#searchicon')
-        .mouseover(function () {
-        $(this).attr("src", "img/icons/42x42/charcoal/42x42-search.png");
-    })
-        .mouseout(function () {
-        $(this).attr("src", "img/icons/42x42/mint/42x42-search.png");
-    });
-         $('#lightboxes')
-        .mouseover(function () {
-        $(this).attr("src", "img/icons/64x64/charcoal/64x64-lightbox.png");
-    })
-        .mouseout(function () {
-        $(this).attr("src", "img/icons/64x64/white/64x64-lightbox.png");
-    });
-         $('#helpdesk')
-        .mouseover(function () {
-        $(this).attr("src", "img/icons/64x64/charcoal/64x64-help-desk.png");
-    })
-        .mouseout(function () {
-        $(this).attr("src", "img/icons/64x64/white/64x64-help-desk.png");
-    });
-         $('#tutorials')
-        .mouseover(function () {
-        $(this).attr("src", "img/icons/64x64/charcoal/64x64-tutorials.png");
-    })
-        .mouseout(function () {
-        $(this).attr("src", "img/icons/64x64/white/64x64-tutorials.png");
-    });
+        .mouseover(function() {
+            $(this).attr("src", "img/icons/42x42/charcoal/42x42-search.png");
+        })
+        .mouseout(function() {
+            $(this).attr("src", "img/icons/42x42/mint/42x42-search.png");
+        });
+    $('#lightboxes')
+        .mouseover(function() {
+            $(this).attr("src", "img/icons/64x64/charcoal/64x64-lightbox.png");
+        })
+        .mouseout(function() {
+            $(this).attr("src", "img/icons/64x64/white/64x64-lightbox.png");
+        });
+    $('#helpdesk')
+        .mouseover(function() {
+            $(this).attr("src", "img/icons/64x64/charcoal/64x64-help-desk.png");
+        })
+        .mouseout(function() {
+            $(this).attr("src", "img/icons/64x64/white/64x64-help-desk.png");
+        });
+    $('#tutorials')
+        .mouseover(function() {
+            $(this).attr("src", "img/icons/64x64/charcoal/64x64-tutorials.png");
+        })
+        .mouseout(function() {
+            $(this).attr("src", "img/icons/64x64/white/64x64-tutorials.png");
+        });
 });
