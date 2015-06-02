8443316160841687d8b3bf269202222651e845f7
sneha Vankireddy <vankireddy.s@husky.neu.edu>
2015-06-02 10:49:58 -0400

    Adding hovering effects
    Adding hovering effects

--- js/myScript.js
+++ js/myScript.js
@@ -15,6 +15,37 @@ $('.prev').click(function() {
 $('.next').click(function() {
   append();
 });
+
 function startTimer() {
-              setInterval(append, 9000);
-          }
+  setInterval(append, 9000);
+}
+$(document).ready(function () {
+    $('#searchicon')
+        .mouseover(function () {
+        $(this).attr("src", "img/icons/42x42/charcoal/42x42-search.png");
+    })
+        .mouseout(function () {
+        $(this).attr("src", "img/icons/42x42/mint/42x42-search.png");
+    });
+         $('#lightboxes')
+        .mouseover(function () {
+        $(this).attr("src", "img/icons/64x64/charcoal/64x64-lightbox.png");
+    })
+        .mouseout(function () {
+        $(this).attr("src", "img/icons/64x64/white/64x64-lightbox.png");
+    });
+         $('#helpdesk')
+        .mouseover(function () {
+        $(this).attr("src", "img/icons/64x64/charcoal/64x64-help-desk.png");
+    })
+        .mouseout(function () {
+        $(this).attr("src", "img/icons/64x64/white/64x64-help-desk.png");
+    });
+         $('#tutorials')
+        .mouseover(function () {
+        $(this).attr("src", "img/icons/64x64/charcoal/64x64-tutorials.png");
+    })
+        .mouseout(function () {
+        $(this).attr("src", "img/icons/64x64/white/64x64-tutorials.png");
+    });
+});
